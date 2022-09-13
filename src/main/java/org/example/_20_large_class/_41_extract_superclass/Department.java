f26c3d937ddaabbdbb2cf99aea98cb7a36762fdf
JayPark7821 <pjhyun1990@gmail.com>
2022-09-14 07:59:20 +0900

    냄새 20, 거대한 클래스 슈퍼클래스 추출하기

---
+++ src/main/java/org/example/_20_large_class/_41_extract_superclass/Department.java
@@ -0,0 +1,26 @@
+package org.example._20_large_class._41_extract_superclass;
+
+import java.util.List;
+
+public class Department extends Party{
+
+    private List<Employee> staff;
+
+    public Department(String name) {
+        super(name);
+    }
+
+
+    public List<Employee> getStaff() {
+        return staff;
+    }
+
+    @Override
+    public double monthlyCost() {
+        return this.staff.stream().mapToDouble(Party::monthlyCost).sum();
+    }
+
+    public int headCount() {
+        return this.staff.size();
+    }
+}
