b42ef662a9c093e41e1a7ebbe9390eb108706ed4
JayPark7821 <pjhyun1990@gmail.com>
2022-09-15 19:22:38 +0900

    냄새 22, 데이터 클래스 레코드 캡슐화 하기

---
+++ src/main/java/org/example/_22_data_class/package-info.java
@@ -0,0 +1,1 @@
+package org.example._22_data_class;
---
+++ src/test/java/org/example/_22_data_class/_42_encapsulate_record/OrganizationTest.java
@@ -0,0 +1,17 @@
+package org.example._22_data_class._42_encapsulate_record;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizationTest {
+
+    @Test
+    void name() {
+        Organization organization = new Organization();
+        organization.name = "jay";
+    }
+
+
+
+}
